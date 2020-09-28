31b53a6b3a48f1a5646d6dc50784318f8fe5d6e5
spaspa <spa@spaspa.dev>
2020-09-28 23:55:52 +0900

    update navvar

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,6 +2,11 @@ module.exports = {
   lang: "ja",
   title: "SysAd Design Guideline",
   themeConfig: {
-    sidebar: [{text: "po", link: "/po"}]
+    sidebar: [
+      {text: "Brand", link: "/docs/foundations/brand"},
+      {text: "Color", link: "/docs/foundations/color"},
+      {text: "Layout", link: "/docs/foundations/layout"},
+      {text: "traQ", link: "/docs/services/traQ"}
+    ]
   }
 }
